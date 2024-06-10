4c5615e86daae41481c0ee4b4729f7f8e8e7f586
longdangquoc <longdq@vnpt.vn>
2024-06-10 17:55:04 +0700

    update kafka

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Customer.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Customer.java
@@ -3,10 +3,11 @@ package com.food.ordering.system.order.service.domain.entity;
 import com.food.ordering.system.domain.entity.AggregateRoot;
 import com.food.ordering.system.domain.valo.CustomerId;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 public class Customer extends AggregateRoot<CustomerId> {
 
